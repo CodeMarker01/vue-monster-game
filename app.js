c04806098ee3af8dcfffd7c5585733c181dba187
quangtran98 <quangtd@dgroup.co>
2021-09-12 21:55:07 +0700

    382. Implementing a 'Special Attack' and 'heal'

--- app.js
+++ app.js
@@ -15,18 +15,37 @@ new Vue({
     },
     attack() {
       this.monsterHealth -= this.calculateDamage(3, 10);
+      console.log(this.calculateDamage(3, 10));
       if (this.checkWin()) {
         return;
       }
       //   damage = Math.round((max * (Math.random() + min)) / (1 + min));
       //   let damege = Math.max(Math.floor(Math.random() * max) + 1, min);
-      this.playerHealth -= this.calculateDamage(5, 12);
-      this.checkWin();
+      this.monsterAttack();
+    },
+    specialAttack() {
+      this.monsterHealth -= this.calculateDamage(10, 15);
+      if (this.checkWin()) {
+        return;
+      }
+      this.monsterAttack();
+    },
+    heal() {
+      if (this.playerHealth <= 90) {
+        this.playerHealth += 10;
+      } else {
+        this.playerHealth = 100;
+      }
+      this.monsterAttack();
     },
 
     // helper function
+    monsterAttack() {
+      this.playerHealth -= this.calculateDamage(5, 12);
+      this.checkWin();
+    },
     calculateDamage(min, max) {
-      return Math.round((max * (Math.random() + min)) / (1 + min));
+      return Math.floor(Math.random() * (max - min + 1) + min);
     },
     checkWin() {
       if (this.monsterHealth <= 0) {
