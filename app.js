cc59e97d380d2d2c49e15feaa034ae6f79daa8fa
quangtran98 <quangtd@dgroup.co>
2021-09-12 17:35:25 +0700

    380. Implementing a Attack Method

--- app.js
+++ app.js
@@ -12,5 +12,27 @@ new Vue({
       this.playerHealth = 100;
       this.monsterHealth = 100;
     },
+    attack() {
+      let max = 10;
+      let min = 3;
+      let damage = Math.round((max * (Math.random() + min)) / (1 + min));
+      //   let damege = Math.max(Math.floor(Math.random() * max) + 1, min);
+      this.monsterHealth -= damage;
+      if (this.monsterHealth <= 0) {
+        alert("Yon won!");
+        this.gameIsRunning = false;
+        return;
+      }
+      max = 12;
+      min = 5;
+      damage = Math.round((max * (Math.random() + min)) / (1 + min));
+      //   let damege = Math.max(Math.floor(Math.random() * max) + 1, min);
+      this.playerHealth -= damage;
+      if (this.playerHealth <= 0) {
+        alert("Yon lost!");
+        this.gameIsRunning = false;
+        return;
+      }
+    },
   },
 });
